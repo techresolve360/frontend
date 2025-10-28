a9b64f414aba885156a1448569f66b34be7b92a2
nchikt <nachiketmahajan15963@gmail.com>
2025-10-28 15:14:17 +0530

    nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { domains: ['d2uvjal4nw1o1a.cloudfront.net'] }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d2uvjal4nw1o1a.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
